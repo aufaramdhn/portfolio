c7936ad65c677ffd31a7a4ee7eabf62450aabdf3
aufaramdhn <ramadhanaufa3@gmail.com>
2023-09-02 14:48:07 +0700

    update all

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
-import { TypeWriter } from "../components/Typewriter";
+import { TypeWriter } from "../components/Typewriter.jsx";
 import Avatar from "/profile/avatar.png";
 import { RiDownload2Line } from "react-icons/ri";
-import { SocialMedia } from "../components/SocialMedia";
+import { SocialMedia } from "../components/SocialMedia.jsx";
 
 export const Home = () => {
   return (
