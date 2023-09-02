08efa71904107093777180befae312c12151205c
aufaramdhn <ramadhanaufa3@gmail.com>
2023-09-02 16:56:52 +0700

    handle error deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Navbar } from "./components/Navbar.jsx";
-import { Home } from "./containers/Home.jsx";
-import { About } from "./containers/About.jsx";
-import { Skills } from "./containers/Skills.jsx";
-import { Footer } from "./components/Footer.jsx";
-import { Project } from "./containers/Project.jsx";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./containers/Home";
+import { About } from "./containers/About";
+import { Skills } from "./containers/Skills";
+import { Footer } from "./components/Footer";
+import { Project } from "./containers/Project";
 
 export default function App() {
   return (
